bb48c6c2e75519830697ee4d5040e755551d5043
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2020-12-29 16:19:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,18 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         firstword = request.form['firstword']
-        return 'The ' + firstword + ' punched him!'
+        secondword = request.form['secondword']
+        thirdword = request.form['thirdword']
+        fourthword = request.form['fourthword']
+        fifthword = request.form['fifthword']
+        sixthword = request.form['sixthword']
+        seventhword = request.form['seventhword']
+        eighthword = request.form['eighthword']
+        ninthword = request.form['ninthword']
+        tenthword = request.form['tenthword']
+        eleventhword = request.form['eleventhword']
+        twelvethword = request.form['twelvethword']
+        return 'The charge against me is a ' + firstword +' with one ' + secondword + ' for purposes of improper ' + thirdword + ' speculation. My real crime is an ' + fourthword + ' ' + fifthword + ' with his ' + sixthword + ', for a ' + seventhword + ' time with his privity and ' + eighthword + ', if not originally brought on by a combination between the ' + ninthword + ' and ' + tenthword + ' with the design to ' + eleventhword + ' ' + twelvethword +' from me.'
 
 
     else:
