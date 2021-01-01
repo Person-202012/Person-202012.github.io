6b7296372b0ec969e48202de6cf5a7d65a6df88f
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2021-01-01 11:16:07 -0400

    Create madlib1.js

---
+++ madlib1.js
@@ -0,0 +1,16 @@
+function madLib1() {
+    var firstWord = document.querySelector('#firstword').value;
+    var secondWord = document.querySelector('#secondword').value;
+    var thirdWord = document.querySelector('#thirdword').value;
+    var fourthWord = document.querySelector('#fourthword').value;
+    var fifthWord = document.querySelector('#fifthword').value;
+    var sixthWord = document.querySelector('#sixthword').value;
+    var seventhWord = document.querySelector('#seventhword').value;
+    var eighthWord = document.querySelector('#eighthword').value;
+    var ninthWord = document.querySelector('#ninthword').value;
+    var tenthWord = document.querySelector('#tenthword').value;
+    var eleventhWord = document.querySelector('#eleventhword').value;
+    var twelvethWord = document.querySelector('#twelvethword').value;
+
+    document.querySelector('#result').innerHTML = 'The charge against me is a ' + firstWord +' with one ' + secondWord + ' for purposes of improper ' + thirdWord + ' speculation. My real crime is an ' + fourthWord + ' ' + fifthWord + ' with his ' + sixthWord + ', for a ' + seventhWord + ' time with his privity and ' + eighthWord + ', if not originally brought on by a combination between the ' + ninthWord + ' and ' + tenthWord + ' with the design to ' + eleventhWord + ' ' + twelvethWord +' from me.';
+}
