01b2019a4f2bce35a86eb1a383727e78e64312e9
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2021-02-18 16:34:06 -0400

    Update Riddle1.js

--- js/Riddle1.js
+++ js/Riddle1.js
@@ -1,34 +1,13 @@
-let toggleNavStatus = false;
-
-let toggleNav = function() {
-    let getSidebar = document.querySelector(".nav-sidebar");
-    let getSidebarUl = document.querySelector(".nav-sidebar ul");
-    let getSidebarTitle = document.querySelector(".nav-sidebar ul li span");
-    let getSidebarLinks = document.querySelectorAll(".nav-sidebar ul li a");
-
-    if (toggleNavStatus === false) {
-        getSidebarUl.style.visibility = 'visible';
-        getSidebar.style.width = '272px';
-        getSidebarTitle.style.opacity = '0.5';
-
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength; i++) {
-            getSidebarLinks[i].style.opacity = "1";
-        }
-
-        toggleNavStatus = true;
-    
-    } else if (toggleNavStatus === true) {
-
-        getSidebar.style.width = '50px';
-        getSidebarTitle.style.opacity = '0';
-
-        let arrayLength = getSidebarLinks.length;
-        for (let i = 0; i < arrayLength; i++) {
-            getSidebarLinks[i].style.opacity = "0";
-        }
-        getSidebarUl.style.visibility = 'hidden';
-
-        toggleNavStatus = false;
-    }
+function Person(name, eyeColor, age) {
+    this.name = name;
+    this.eyeColor = eyeColor
+    this.age = age;
+    this.updateAge = function() {
+        return ++this.age;
+    };
 }
+
+
+let person01 = new Person('Neil', 'brown', 13);
+
+console.log(person01.updateAge())
