698e9fe988c1f84fa6b22a8851d4f8a056afdf97
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2021-04-30 13:07:12 -0300

    Create main.js

---
+++ Neil_ELA_Final_Project/js/main.js
@@ -0,0 +1,8 @@
+let main = {
+    setGameStart: function() {
+        let getEnemyHealth = document.querySelector('.health-enemy');
+        getEnemyHealth.innerHTML = '<p class="health-enemy">Health: ' + setEnemyHealth + '</p>';
+    }
+    
+}
+   
