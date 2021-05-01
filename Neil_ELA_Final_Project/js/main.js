11de6e53aa04116114121284f9ec7900a7924ee9
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2021-05-01 15:27:18 -0300

    Update main.js

--- Neil_ELA_Final_Project/js/main.js
+++ Neil_ELA_Final_Project/js/main.js
@@ -1,9 +1,11 @@
 let main = {
 
     start: function() {
-        let imageRn1 = document.querySelector('.displayWindow');
-        imageRn1.innerHTML = '<img src="img/marsplanet2.jpg" alt="marssomecrater" class="img1">';
+        let imageRn1 = document.querySelector('.img1');
+        imageRn1.style
     }
     
 }
-   
+let imageRn1 = document.querySelector('.displayWindow');
+
+imageRn1.innerHTML = '<img src="img/marsplanet2.jpg" alt="marssomecrater" class="img1">';
