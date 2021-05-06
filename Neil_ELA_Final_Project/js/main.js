a770397384d85e80d0438cb71791b3fdd393c705
Person-202012 <70764939+Person-202012@users.noreply.github.com>
2021-05-06 10:22:20 -0300

    Update main.js

--- Neil_ELA_Final_Project/js/main.js
+++ Neil_ELA_Final_Project/js/main.js
@@ -3,14 +3,22 @@ let main = {
     start: function() {
         //let startButton = document.getElementById('startButton');
         //startButton.remove();
-        let imageRn1 = document.querySelector('#testimg1');
+        console.log('getting ready to change top!');
+        let imageRn1 = document.querySelector('#testimg1')
+        console.log(imageRn1);
+        
         //imageRn1.style.transform = 'scale(33)'
         //imageRn1.style.transform = 'object-position(100%, 0)'
-        imageRn1.style.left = '100px';
+        //imageRn1.style.transform = "top(1000000px)";
+        let container = document.getElementById('container')
+
+        container.innerHTML = '<img src="img/marsplanet2.jpg" alt="marssomecrater" id="testimg2">';
+        let imageRn2 = document.querySelector("#testimg2");
+        console.log(imageRn2);
 
     }
     
 }
-let imageRn1 = document.querySelector('.displayWindow');
+//let imageRn1 = document.querySelector('.displayWindow');
 
-imageRn1.innerHTML = '<img src="img/marsplanet2.jpg" alt="marssomecrater" class="img1">';
+//
